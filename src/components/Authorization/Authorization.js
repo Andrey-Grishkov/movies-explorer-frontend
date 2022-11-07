f826b9ad9536208ca9b0bbbe090939a66a662873
Андрей Гришков <andreygorm@gmail.com>
2022-11-07 09:29:55 +0300

    refactoring style avtorization

--- src/components/Authorization/Authorization.js
+++ src/components/Authorization/Authorization.js
@@ -4,48 +4,47 @@ import Logo from "../Logo/Logo";
 
 function Authorization(props) {
   return (
-    <section className='register'>
-      <div className='register__header'>
-        <div className='register__header-container'>
-          <div className='register__logo-container'>
+    <section className='authorization'>
+      <div className='authorization__header'>
+        <div className='authorization__header-container'>
+          <div className='authorization__logo-container'>
             <Logo />
           </div>
-          <p className='register__text'>{props.title}</p>
+          <p className='authorization__text'>{props.title}</p>
         </div>
       </div>
       <form>
-      <ul className='register__container'>
+      <ul className='authorization__container'>
         {props.authType === 'register' && (
-        <li className='register__part-form'>
-          <p className='register__name'>Имя</p>
-          <input className='register__input' placeholder='Имя'/>
+        <li className='authorization__part-form'>
+          <p className='authorization__name'>Имя</p>
+          <input className='authorization__input' placeholder='Имя'/>
         </li>)}
-        {props.authType === 'register' && (<p className='register__validation-text'>что-то пошло не так...</p>
+        {props.authType === 'register' && (<p className='authorization__validation-text'>что-то пошло не так...</p>
           )}
-        <li className='register__part-form'>
-          <p className='register__name'>E-mail</p>
-          <input className='register__input' placeholder='E-mail'/>
+        <li className='authorization__part-form'>
+          <p className='authorization__name'>E-mail</p>
+          <input className='authorization__input' placeholder='E-mail'/>
         </li>
-        <p className='register__validation-text'>что-то пошло не так...</p>
-        <li className='register__part-form'>
-          <p className='register__name'>Пароль</p>
-          <input className='register__input' placeholder='Пароль' type='password'/>
+        <p className='authorization__validation-text'>что-то пошло не так...</p>
+        <li className='authorization__part-form'>
+          <p className='authorization__name'>Пароль</p>
+          <input className='authorization__input' placeholder='Пароль' type='password'/>
         </li>
-        <p className='register__validation-text'>что-то пошло не так...</p>
+        <p className='authorization__validation-text'>что-то пошло не так...</p>
       </ul>
       </form>
-
-
-
-      <div className='register__confirm'>
-        <button className='register__confirm-button' type='submit'>{props.button}</button>
-        <div className='register__confirm-container'>
-          <p className='register__confirm-text'>{props.text}</p>
-          <a className='register__confirm-link' href={props.linkRout}>{props.link}</a>
+      <div className='authorization__confirm'>
+        <button className={`${props.authType === 'register' ? 'authorization__confirm-button' : 'authorization__confirm-button authorization__confirm-button-login'}`} type='submit'>{props.button}</button>
+        <div className='authorization__confirm-container'>
+          <p className='authorization__confirm-text'>{props.text}</p>
+          <a className='authorization__confirm-link' href={props.linkRout}>{props.link}</a>
         </div>
       </div>
     </section>
   );
 }
+// {`${props.authType === 'register' ? 'authorization__confirm-button' : 'authorization__confirm-button authorization__confirm-button-login'}`
+// className={`${isErr ? "authorizatio__input authorizatio__input_active" : "authorizatio__input"}`
 
 export default Authorization;
